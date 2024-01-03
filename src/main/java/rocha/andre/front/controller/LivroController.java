f98da8126ba863760273d191d2b1b17aa804a5f8
andrelrocha <andrerocha0911@gmail.com>
2024-01-03 12:50:38 -0300

    feat: telas de login e cadastro

---
+++ src/main/java/rocha/andre/front/controller/LivroController.java
@@ -0,0 +1,34 @@
+package rocha.andre.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LivroController {
+    @GetMapping("/livros")
+    public String listar() {
+        return "livro/listalivros";
+    }
+
+    @GetMapping("/livros/livroporid")
+    public String listarPorId() {
+        return "livro/livro";
+    }
+
+    @GetMapping("/livros/criar")
+    public String create() {
+        return "livro/create";
+    }
+
+    @GetMapping("/livros/update")
+    public String updateLivro() {
+        return "livro/update";
+    }
+
+    @GetMapping("/livros/emprestimos")
+    public String emprestimoLivro() {
+        return "livro/listaemprestimoslivro";
+    }
+
+}
+
---
+++ src/main/java/rocha/andre/front/controller/UserController.java
@@ -0,0 +1,27 @@
+package rocha.andre.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/users/create")
+    public String create() {
+        return "user/create";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "user/create";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login";
+    }
+
+    @GetMapping("/")
+    public String homepage() {
+        return "user/login";
+    }
+}
