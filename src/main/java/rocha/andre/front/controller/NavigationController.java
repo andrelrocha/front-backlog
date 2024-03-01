2ae552fa81c987fc248d585f4526c9df18531e5e
andrelrocha <andrerocha0911@gmail.com>
2024-03-01 12:31:27 -0300

    feat: tela de navegação (opinioes box)

--- src/main/java/rocha/andre/front/controller/NavigationController.java
+++ src/main/java/rocha/andre/front/controller/NavigationController.java
@@ -3,10 +3,10 @@ package rocha.andre.front.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller("/navigation")
+@Controller
 public class NavigationController {
     @GetMapping("/afterlogin")
     public String afterLogin() {
-        return "navigation/afterlogin";
+        return "navigation/after_login";
     }
 }
---
+++ src/main/resources/static/js/navigation/linksAfterLogin.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const opinionsNavigation = document.getElementById('opinionsNavigation');
+
+    opinionsNavigation.addEventListener('click', function () {
+        window.location.href = '/opinioes';
+    });
+});
